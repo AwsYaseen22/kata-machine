1ae85e75c423076f9f0166f936f81f4b9a27fb37
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-11-13 23:43:12 +0300

    Bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,13 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length-i-1; j++) {
+            if(arr[j]>arr[j+1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]]
+            }
+        }
+    }
 }
+
+// let arr = [1,3,5,4,7,6,2]
+// bubble_sort(arr)
+// console.log(arr);
