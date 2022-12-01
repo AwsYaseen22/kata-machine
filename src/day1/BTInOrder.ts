5c7b510aaae58cfba2ae0ae432e34452f5f0a6d5
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-12-01 21:27:47 +0300

    binary tree In Order

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,21 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
 
+function walk(cur: BinaryNode<number> | null, path: number[]):number[] {
+    if(!cur){
+        return path;
+    }
+    
+    // pre recurse
+    
+    // recurse
+    walk(cur.left, path);
+    // push the value after there will be not left anymore
+    path.push(cur.value)
+    walk(cur.right, path);
+    
+    // post recurse
+    return path;
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, [])
 }
