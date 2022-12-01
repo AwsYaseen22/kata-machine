0ac1c99253186ad810847c22218b09b427f6bb9f
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-12-01 21:22:24 +0300

    binary tree pre order

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,20 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
 
+function walk(cur: BinaryNode<number> | null, path: number[]):number[] {
+    if(!cur){
+        return path;
+    }
+    
+    // pre recurse
+    path.push(cur.value)
+    
+    // recurse
+    walk(cur.left, path);
+    walk(cur.right, path);
+    
+    // post recurse
+    return path;
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, [])
 }
