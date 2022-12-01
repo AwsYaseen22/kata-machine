fd60512644926f00a8c1fd0c837c1a917a5cafb1
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-12-01 21:30:47 +0300

    binary tree Post Order

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,21 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(cur: BinaryNode<number> | null, path: number[]):number[] {
+    if(!cur){
+        return path;
+    }
+    
+    // pre recurse
+    
+    // recurse
+    walk(cur.left, path);
+    walk(cur.right, path);
+    // push the value after there will be not left and no right anymore
+    path.push(cur.value)
+    
+    // post recurse
+    return path;
+}
 
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
 }
