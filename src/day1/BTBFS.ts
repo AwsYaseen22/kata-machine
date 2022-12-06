f065310033649c2b8c9abc5b71be97d58bf69724
AwsYaseen22 <aws.yaseen.22@gmail.com>
2022-12-06 22:48:49 +0300

    solve binary tree breadth first search BTF

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,29 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-
+    // const q = [head];
+    // while (q.length) {
+    //     const cur = q.shift();
+    //     if(cur?.value === needle){
+    //         return true;
+    //     }
+    //     if(cur?.left){
+    //         q.push(cur.left);
+    //     }
+    //     if(cur?.right){
+    //         q.push(cur.right);
+    //     }
+    // }
+    // return false;
+    const q: (BinaryNode<number> | null)[] = [head];
+    while (q.length) {
+        const cur = q.shift() as BinaryNode<number> | undefined | null;
+        if(!cur){
+            continue;
+        }
+        if(cur.value === needle){
+            return true;
+        }
+        q.push(cur.left);
+        q.push(cur.right);
+    }
+    return false;
 }
